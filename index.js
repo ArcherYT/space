9adb19ef98edfc68b80b9207b76bbbbe00c4796f
ArcherYT <72284744+ArcherYT@users.noreply.github.com>
2021-01-03 12:06:16 +1100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ client.aliases = new discord.Collection();
 ["command", "events"].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
+client.on("guildMemberAdd", (member) => { //usage of welcome event
+  let chx = db.get(`welchannel_${member.guild.id}`); //defining var
+  
+  if(chx === null) { //check if var have value or not
+    return;
+  }
+
+  let wembed = new discord.MessageEmbed() //define embed
+  .setAuthor(member.user.username, member.user.avatarURL())
+  .setColor("#ff2050")
+  .setThumbnail(member.user.avatarURL())
+  .setDescription(`We are very happy to have you in our server`);
+  
+  client.channels.cache.get(chx).send(wembed) //get channel and send embed
+})
 
 
 client.login(token);
